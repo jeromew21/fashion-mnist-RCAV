df9475e3b9040a648652cac5a5bb68e0827fbef2
Jerome Wei <jeromew@berkeley.edu>
2020-05-27 01:37:00 -0700

    made some changes

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import matplotlib.pyplot as plt
 import struct
 import os
+import cv2
+import sys
 import numpy as np
 from PIL import Image
-
 import random
+
+random.seed(1)
 
 PATH_TO_TEXTURES = "textures"
 THRESHOLD = 10
+
+DELTAS = (
+    (-1, 0),
+    (-1, 1),
+    (-1, -1),
+    (1, 0),
+    (1, 1),
+    (1, -1),
+    (0, 1),
+    (0, -1)
+)
+SIZE = 28
 
 def binary_threshold(x):
     if x < THRESHOLD:
@@ -20,22 +35,23 @@ def read_idx(filename):
         shape = tuple(struct.unpack('>I', f.read(4))[0] for d in range(dims))
         return np.frombuffer(f.read(), dtype=np.uint8).reshape(shape)
 
-def in_bounds(coords, w, h):
-    return all((coords[0] >= 0, coords[0] < h, coords[1] >= 0, coords[1] < w))
+def in_bounds(coords):
+    return coords[0] >= 0 and coords[0] < SIZE and coords[1] >= 0 and coords[1] < SIZE
 
 def is_boundary_pixel(coords, bitmap):
-    for deltaX in (-1, 0, 1):
-        for deltaY in (-1, 0, 1):
-            if deltaX or deltaY:
-                neighbors = (coords[0] + deltaX, coords[1] + deltaY)
-                if in_bounds(neighbors, bitmap.shape[1], bitmap.shape[0]):
-                    if bitmap[neighbors[1]][neighbors[0]] == 0:
-                        return True
-                else:
-                    return True
+    x = coords[0]
+    y = coords[1]
+    for deltaX, deltaY in DELTAS:
+        neighbors = (x + deltaX, y + deltaY)
+        if in_bounds(neighbors):
+            if bitmap[neighbors[1]][neighbors[0]] == 0:
+                return True
+        else:
+            return True
     return False
 
 def inside_boundary(coord, boundary_set):
+    #hotspot
     x0 = coord[0]
     y0 = coord[1]
     values = [False, False, False, False]
@@ -45,15 +61,15 @@ def inside_boundary(coord, boundary_set):
         x = x0
         y = y0
         while True:
-            coord = (x, y)
-            if in_bounds(coord, boundary_set.shape[1], boundary_set.shape[0]):
-                if boundary_set[coord[1]][coord[0]] == 1:
+            if in_bounds((x, y)):
+                if boundary_set[y][x] == 1:
                     values[iv] = True
+                    break
                 x += dx
                 y += dy
             else:
                 break
-    return all(values)
+    return values[0] and values[1] and values[2] and values[3]
 
 def fill_bitmap(arr):
     #given a numpy array, return a filled version
@@ -63,34 +79,41 @@ def fill_bitmap(arr):
     for y in range(height):
         for x in range(width):
             binary_set[y][x] = binary_threshold(binary_set[y][x])
+
     #run DFS to find disjoint sets
     visited = np.zeros((height, width))
     def explore(coords, _set):
         visited[coords[1]][coords[0]] = 1
         _set.add(coords)
         color = binary_set[coords[1]][coords[0]]
-        for deltaX in (-1, 0, 1):
-            for deltaY in (-1, 0, 1):
-                if deltaX or deltaY:
-                    neighbors = (coords[0] + deltaX, coords[1] + deltaY)
-                    if in_bounds(neighbors, width, height) \
-                        and color == binary_set[neighbors[1]][neighbors[0]] \
-                        and visited[neighbors[1]][neighbors[0]] == 0:
-                        explore(neighbors, _set)
+        for deltaX, deltaY in DELTAS:
+            neighbors = (coords[0] + deltaX, coords[1] + deltaY)
+            if in_bounds(neighbors) \
+                and color == binary_set[neighbors[1]][neighbors[0]] \
+                and visited[neighbors[1]][neighbors[0]] == 0:
+                explore(neighbors, _set)
 
     disjoint_sets = []
     for y in range(height):
+        flag = False
         for x in range(width):
             if visited[y][x] == 0:
                 _set = set()
                 explore((x, y), _set)
                 disjoint_sets.append(_set)
+                test_item = next(iter(_set))
+                if len(_set) > 392 and binary_set[test_item[1]][test_item[0]] == 255:
+                    flag = True
+                    break
+        if flag:
+            break
     
     def key(_set):
-        coord = list(_set)[0]
+        coord = next(iter(_set))
         if binary_set[coord[1]][coord[0]] == 255:
             return len(_set)
         return 0
+
     max_set = max(disjoint_sets, key=key)
     plt_arr = np.zeros((height, width))
     result = np.zeros((height, width))
@@ -100,7 +123,7 @@ def fill_bitmap(arr):
     for coord in max_set:
         plt_arr[coord[1]][coord[0]] = 200
         result[coord[1]][coord[0]] = 255   
-        if is_boundary_pixel(coord, binary_set):
+        if is_boundary_pixel(coord, binary_set): #hotspot
             boundary_set[coord[1]][coord[0]] = 1
             plt_arr[coord[1]][coord[0]] = 255
 
@@ -120,9 +143,19 @@ def fill_bitmap(arr):
     return result
 
 def load_texture(filename):
-    im = Image.open(filename)
-    arr = np.array(im)
-    return arr
+    print(filename)
+    im = np.array(Image.open(filename))
+    return im
+
+def scale_texture(texture, scale=1):
+    if scale > 1 or scale < 0:
+        raise Exception("invalid scale: must be in [0, 1]") 
+    size_new = int(32 + ((256-32)*(1-scale)))
+    res = cv2.resize(texture, dsize=(size_new, size_new), interpolation=cv2.INTER_CUBIC)
+    #crop to center
+    resize = (size_new-32)//2 
+    return res[resize:resize+32, resize:resize+32]
+
 
 def add_to_filled(filled, texture, offset=(0,0)):
     result = np.zeros(filled.shape)
@@ -137,16 +170,17 @@ def add_to_filled(filled, texture, offset=(0,0)):
         ry += 1
     return result
 
-def interpolate_textures(t1, t2):
+def interpolate_textures(texture_list):
+    t1 = texture_list[0]
     result = np.zeros(t1.shape)
     for y in range(t1.shape[0]):
         for x in range(t1.shape[1]):
-            val = int((int(t1[y][x]) + int(t2[y][x]))/2) 
+            val = int(
+                sum((int(t[y][x]) for t in texture_list)) / len(texture_list)
+            )
             #Watch out for overflow
-            #   convert to int explicitly bfore interpolation
+            #   convert to int explicitly before interpolation
             result[y][x] = val
-    show_bitmap_sbs(t1, t2)
-    show_bitmap(result)
     return result
 
 def show_bitmap(arr1):
@@ -159,20 +193,54 @@ def show_bitmap_sbs(arr1, arr2):
     plt.imshow([np.concatenate([arr1[i], arr2[i]]) for i in range(arr1.shape[0])], cmap='gray')
     plt.show()
 
-def test():
-    imgs = read_idx("train-images-idx3-ubyte")
-    labels = read_idx("train-labels-idx1-ubyte")
-    textures = [os.path.join(PATH_TO_TEXTURES, i) for i in os.listdir(PATH_TO_TEXTURES)]
-    print(textures)
-    
+def add_noise(arr, intensity=0):
+    NOISE_LEVEL = 64
+    result = np.zeros(arr.shape)
+    for y in range(arr.shape[0]):
+        for x in range(arr.shape[1]):
+            delta = int(intensity * random.randint(-NOISE_LEVEL, NOISE_LEVEL))
+            val = arr[y][x] + delta
+            result[y][x] = max(0, min(255, val))
+    return result
 
-    for i in range(100):
+class TexturedFMNIST():
+    def __init__(self, texture_dir=PATH_TO_TEXTURES, fmnist_dir='.'):
+        self.imgs = read_idx(os.path.join(fmnist_dir, "train-images-idx3-ubyte"))
+        self.labels = read_idx(os.path.join(fmnist_dir, "train-labels-idx1-ubyte"))
+
+
+        self.textures = [load_texture(os.path.join(texture_dir, i)) for i in os.listdir(texture_dir)]
+
+
+    def build_class(self, class_num, texture_choices=[], texture_interpolation=0, texture_rescale=True, texture_aug=True, aug_intensity=0.5):
+        '''
+        texture_rescale: either bool or float oor string? controls how to rescale textures when applying
+        texture_aug: whether to add some noise to texture scaling and orientation before application
+        '''
+        # Loop over self.get_textured_sample randomly sampling noise for texture application if texture_aug is True
+        return #[np arrays of images with textures applied]
+
+    def get_textured_sample(self, img_index, sample_fname, texture_choices=[], texture_interpolation=0, texture_rescale=True, texture_aug=True):
+        offset = (0, 0)
+        noise = 0
+        if texture_aug:
+            offset = (random.randint(0, 4), random.randint(0, 4))
+            noise = 1
+            
+        texture = interpolate_textures([
+            add_noise(scale_texture(random.choice(self.textures)), noise),
+            add_noise(scale_texture(random.choice(self.textures)), noise)
+        ])
+
         i1 = add_to_filled(
-            fill_bitmap(imgs[i]),
-            interpolate_textures(
-                load_texture("textures/zigzag3_small.png"),
-                load_texture("textures/stripes1_small.png")
-            ),
-            offset = (0, 0)
+            fill_bitmap(self.imgs[img_index]), texture, offset
         )
-        show_bitmap(i1)
+        return i1, self.labels[img_index]
+
+#with PyCallGraph(output=GraphvizOutput()):
+ #   test()
+def test():
+    tf = TexturedFMNIST()
+    for i in range(1000):
+        show_bitmap(tf.get_textured_sample(0, False, False)[0])
+        print(i)
